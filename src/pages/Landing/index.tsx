c55f4c1d92d9ce894f07c05a8e03f7e7eb0505a1
Ivan Borba <mrborbaivan@gmail.com>
2022-07-13 13:58:03 -0300

    feature(landing): mounted the first part of the landing page with the side columns

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,10 +1,33 @@
+import { VscGithub } from "react-icons/vsc";
+import { IoLogoLinkedin, IoMailOutline, IoLogoWhatsapp } from "react-icons/io5";
+
 import Header from "../../components/Header";
+import {
+  MainContainer,
+  InfoContainer,
+  SideColumn,
+  VerticalLine,
+} from "./styles";
 
 const LandingPage = () => {
   return (
     <>
       <Header />
-      <div></div>
+      <MainContainer>
+        <SideColumn>
+          <VerticalLine style={{ margin: 0 }} />
+          <VscGithub />
+          <IoLogoLinkedin />
+          <VerticalLine small />
+        </SideColumn>
+        <InfoContainer></InfoContainer>
+        <SideColumn>
+          <VerticalLine small />
+          <IoMailOutline />
+          <IoLogoWhatsapp />
+          <VerticalLine style={{ margin: 0 }} />
+        </SideColumn>
+      </MainContainer>
     </>
   );
 };
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -10,28 +10,43 @@ interface SideColumnProps {
 
 export const VerticalLine = styled.hr`
   border: 1px solid #d2d2d2;
-  transform: rotate(90deg);
   border-radius: 10px;
-  width: 45vw;
+  height: 45vh;
 
   ${(props: VerticalLineProps) =>
     props.small &&
     css`
-      width: 5vw;
+      height: 5vh;
     `}
 `;
 
 export const SideColumn = styled.div`
-  height: 90vh;
   width: 16vw;
+  height: 90vh;
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: space-around;
+  justify-content: space-evenly;
+  background-color: #111111;
 
   ${(props: SideColumnProps) =>
     props.reverse &&
     css`
       flex-direction: column-reverse;
     `}
+`;
+
+export const MainContainer = styled.main`
+  width: 100%;
+  height: 90vh;
+  display: flex;
+`;
+
+export const InfoContainer = styled.div`
+  width: 68vw;
+  height: 90vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #111111;
 `;
